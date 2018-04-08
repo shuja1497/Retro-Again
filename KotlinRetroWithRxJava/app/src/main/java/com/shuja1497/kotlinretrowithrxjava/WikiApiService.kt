2179d74c15687f983d84575340eec6393e07fdda
shuja1497 <shujareshi1497@gmail.com>
2018-04-08 15:32:15 +0530

    Converter(json processor) changed from gson to moshi .

--- KotlinRetroWithRxJava/app/src/main/java/com/shuja1497/kotlinretrowithrxjava/WikiApiService.kt
+++ KotlinRetroWithRxJava/app/src/main/java/com/shuja1497/kotlinretrowithrxjava/WikiApiService.kt
@@ -4,6 +4,7 @@ import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 import java.util.*
@@ -22,7 +23,8 @@ interface WikiApiService {
 
             val retrofit = Retrofit.Builder()
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .addConverterFactory(GsonConverterFactory.create())
+//                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(MoshiConverterFactory.create())
                     .baseUrl("https://en.wikipedia.org/w/")
                     .build()
 
